fa3f0d4e59af794aaa81f457759e07846e45bf4b
chenwenhang <cwh736@qq.com>
2019-11-07 14:07:02 +0800

    feat($model & $view): change some code because the database structure has been refactored

--- kgmilitary/Model/neo4j_models.py
+++ kgmilitary/Model/neo4j_models.py
@@ -11,61 +11,60 @@ class Neo4j_Handle():
         print("Neo4j Init ...")
 
     def connectDB(self):
-        self.graph = Graph("bolt: // localhost:7687", username="neo4j", password="test123456!")
+        self.graph = Graph("bolt: // localhost:7687", user名称="neo4j", password="test123456!")
         self.matcher = NodeMatcher(self.graph)
 
     # 实体查询，用于命名实体识别
     def matchEntityItem(self, value):
-        answer = self.graph.run("MATCH (entity1) WHERE entity1.name = \"" + value + "\" RETURN entity1").data()
+        answer = self.graph.run("MATCH (entity1) WHERE entity1.名称 = \"" + value + "\" RETURN entity1").data()
         return answer
 
     # 实体查询
     def getEntityRelationbyEntity(self, value):
         # 查询实体：不考虑实体类型，只考虑关系方向
         answer = self.graph.run(
-            "MATCH (entity1) - [rel] -> (entity2)  WHERE entity1.name = \"" + value + "\" RETURN rel,entity2").data()
+            "MATCH (entity1) - [rel] -> (entity2)  WHERE entity1.名称 = \"" + value + "\" RETURN rel,entity2").data()
         if (len(answer) == 0):
             # 查询实体：不考虑关系方向
             answer = self.graph.run(
-                "MATCH (entity1) - [rel] - (entity2)  WHERE entity1.name = \"" + value + " \" RETURN rel,entity2").data()
-        print(answer)
+                "MATCH (entity1) - [rel] - (entity2)  WHERE entity1.名称 = \"" + value + " \" RETURN rel,entity2").data()
         return answer
 
     # 关系查询:实体1
     def findRelationByEntity1(self, entity1):
-        answer = self.graph.run("MATCH (n1)- [rel] -> (n2) WHERE n1.name=\"" + entity1 + "\" RETURN n1,rel,n2").data()
+        answer = self.graph.run("MATCH (n1)- [rel] -> (n2) WHERE n1.名称=\"" + entity1 + "\" RETURN n1,rel,n2").data()
         return answer
 
     # 关系查询：实体2
     def findRelationByEntity2(self, entity2):
-        answer = self.graph.run("MATCH (n1)- [rel] -> (n2) WHERE n2.name=\"" + entity2 + "\"  RETURN n1,rel,n2").data()
+        answer = self.graph.run("MATCH (n1)- [rel] -> (n2) WHERE n2.名称=\"" + entity2 + "\"  RETURN n1,rel,n2").data()
         return answer
 
     # 关系查询：实体1+关系
     def findOtherEntities(self, entity, relation):
         answer = self.graph.run(
-            "MATCH (n1)- [rel] -> (n2) WHERE n1.name=\"" + entity + "\" and  rel.name=\""
+            "MATCH (n1)- [rel] -> (n2) WHERE n1.名称=\"" + entity + "\" and  rel.名称=\""
             + relation + "\" RETURN n1,rel,n2").data()
         return answer
 
     # 关系查询：关系+实体2
     def findOtherEntities2(self, entity, relation):
         answer = self.graph.run(
-            "MATCH (n1)- [rel] -> (n2) WHERE n2.name=\"" + entity + "\" and  rel.name=\""
+            "MATCH (n1)- [rel] -> (n2) WHERE n2.名称=\"" + entity + "\" and  rel.名称=\""
             + relation + "\" RETURN n1,rel,n2").data()
         return answer
 
     # 关系查询：实体1+实体2(注意Entity2的空格）
     def findRelationByEntities(self, entity1, entity2):
         answer = self.graph.run(
-            "MATCH (n1)- [rel] -> (n2) WHERE n1.name=\"" + entity1 + "\" and  n2.name=\""
+            "MATCH (n1)- [rel] -> (n2) WHERE n1.名称=\"" + entity1 + "\" and  n2.名称=\""
             + entity2 + "\" RETURN n1,rel,n2").data()
         return answer
 
     # 查询数据库中是否有对应的实体-关系匹配
     def findEntityRelation(self, entity1, relation, entity2):
         answer = self.graph.run(
-            "MATCH (n1)- [rel] -> (n2) WHERE n1.name=\"" + entity1 + "\" and  n2.name=\""
-            + entity2 + "\" and  rel.name=\""
+            "MATCH (n1)- [rel] -> (n2) WHERE n1.名称=\"" + entity1 + "\" and  n2.名称=\""
+            + entity2 + "\" and  rel.名称=\""
             + relation + "\"  RETURN n1,rel,n2").data()
         return answer
--- kgmilitary/kgmilitary/relation_view.py
+++ kgmilitary/kgmilitary/relation_view.py
@@ -20,9 +20,9 @@ with open(filePath + "/toolkit/relationStaticResult.txt", "r") as fr:
 
 def sortDict(relationDict):
     for i in range(len(relationDict)):
-        relationName = relationDict[i]['rel']['type']
+        relationName = relationDict[i]['rel']['name']
         relationCount = relationCountDict.get(relationName)
-        if (relationCount is None):
+        if relationCount is None:
             relationCount = 0
         relationDict[i]['relationCount'] = relationCount
 
@@ -35,7 +35,7 @@ def sortDict(relationDict):
 def search_entity(request):
     ctx = {}
     # 根据传入的实体名称搜索出关系
-    if (request.GET):
+    if request.GET:
         entity = request.GET['user_text']
         # 连接数据库
         db = neo4jconn
@@ -57,59 +57,60 @@ def search_entity(request):
 
 def search_relation(request):
     ctx = {}
-    if (request.GET):
+    if request.GET:
         db = neo4jconn
         entity1 = request.GET['entity1_text']
         relation = request.GET['relation_name_text']
+        if relation == "任意关系":
+            relation = ""
         entity2 = request.GET['entity2_text']
         relation = relation.lower()
         searchResult = {}
         # 若只输入entity1,则输出与entity1有直接关系的实体和关系
-        if (len(entity1) != 0 and len(relation) == 0 and len(entity2) == 0):
+        if len(entity1) != 0 and len(relation) == 0 and len(entity2) == 0:
             searchResult = db.findRelationByEntity1(entity1)
             searchResult = sortDict(searchResult)
-            if (len(searchResult) > 0):
+            if len(searchResult) > 0:
                 return render(request, 'relation.html',
                               {'searchResult': json.dumps(searchResult, ensure_ascii=False)})
 
         # 若只输入entity2则,则输出与entity2有直接关系的实体和关系
-        if (len(entity2) != 0 and len(relation) == 0 and len(entity1) == 0):
+        if len(entity2) != 0 and len(relation) == 0 and len(entity1) == 0:
             searchResult = db.findRelationByEntity2(entity2)
             searchResult = sortDict(searchResult)
-            if (len(searchResult) > 0):
+            if len(searchResult) > 0:
                 return render(request, 'relation.html',
                               {'searchResult': json.dumps(searchResult, ensure_ascii=False)})
         # 若输入entity1和relation，则输出与entity1具有relation关系的其他实体
-        if (len(entity1) != 0 and len(relation) != 0 and len(entity2) == 0):
+        if len(entity1) != 0 and len(relation) != 0 and len(entity2) == 0:
             searchResult = db.findOtherEntities(entity1, relation)
             searchResult = sortDict(searchResult)
-            if (len(searchResult) > 0):
+            if len(searchResult) > 0:
                 return render(request, 'relation.html',
                               {'searchResult': json.dumps(searchResult, ensure_ascii=False)})
         # 若输入entity2和relation，则输出与entity2具有relation关系的其他实体
-        if (len(entity2) != 0 and len(relation) != 0 and len(entity1) == 0):
+        if len(entity2) != 0 and len(relation) != 0 and len(entity1) == 0:
             searchResult = db.findOtherEntities2(entity2, relation)
             searchResult = sortDict(searchResult)
-            if (len(searchResult) > 0):
+            if len(searchResult) > 0:
                 return render(request, 'relation.html',
                               {'searchResult': json.dumps(searchResult, ensure_ascii=False)})
         # 若输入entity1和entity2,则输出entity1和entity2之间的关系
-        if (len(entity1) != 0 and len(relation) == 0 and len(entity2) != 0):
+        if len(entity1) != 0 and len(relation) == 0 and len(entity2) != 0:
             searchResult = db.findRelationByEntities(entity1, entity2)
             searchResult = sortDict(searchResult)
-            if (len(searchResult) > 0):
+            if len(searchResult) > 0:
                 return render(request, 'relation.html',
                               {'searchResult': json.dumps(searchResult, ensure_ascii=False)})
         # 若输入entity1,entity2和relation,则输出entity1、entity2是否具有相应的关系
-        if (len(entity1) != 0 and len(entity2) != 0 and len(relation) != 0):
-            print(relation)
+        if len(entity1) != 0 and len(entity2) != 0 and len(relation) != 0:
             searchResult = db.findEntityRelation(entity1, relation, entity2)
             searchResult = sortDict(searchResult)
-            if (len(searchResult) > 0):
+            if len(searchResult) > 0:
                 return render(request, 'relation.html',
                               {'searchResult': json.dumps(searchResult, ensure_ascii=False)})
         # 全为空
-        if (len(entity1) != 0 and len(relation) != 0 and len(entity2) != 0):
+        if len(entity1) != 0 and len(relation) != 0 and len(entity2) != 0:
             pass
         ctx = {'title': '<h1>暂未找到相应的匹配</h1>'}
         return render(request, 'relation.html', {'ctx': ctx})
